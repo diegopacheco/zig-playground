905d738e9f57cc4e1a9dfe8a0d74093cea2904a2
diegopacheco <diego.pacheco.it@gmail.com>
2023-01-05 19:34:36 -0800

    Zig with package manager zigmod

---
+++ fun-with-zigmod-package-manager/deps.zig
@@ -0,0 +1,86 @@
+// zig fmt: off
+const std = @import("std");
+const builtin = @import("builtin");
+const Pkg = std.build.Pkg;
+const string = []const u8;
+
+pub const cache = ".zigmod/deps";
+
+pub fn addAllTo(exe: *std.build.LibExeObjStep) void {
+    checkMinZig(builtin.zig_version, exe);
+    @setEvalBranchQuota(1_000_000);
+    for (packages) |pkg| {
+        exe.addPackage(pkg.pkg.?);
+    }
+    var llc = false;
+    var vcpkg = false;
+    inline for (comptime std.meta.declarations(package_data)) |decl| {
+        const pkg = @as(Package, @field(package_data, decl.name));
+        for (pkg.system_libs) |item| {
+            exe.linkSystemLibrary(item);
+            llc = true;
+        }
+        for (pkg.frameworks) |item| {
+            if (!std.Target.current.isDarwin()) @panic(exe.builder.fmt("a dependency is attempting to link to the framework {s}, which is only possible under Darwin", .{item}));
+            exe.linkFramework(item);
+            llc = true;
+        }
+        inline for (pkg.c_include_dirs) |item| {
+            exe.addIncludePath(@field(dirs, decl.name) ++ "/" ++ item);
+            llc = true;
+        }
+        inline for (pkg.c_source_files) |item| {
+            exe.addCSourceFile(@field(dirs, decl.name) ++ "/" ++ item, pkg.c_source_flags);
+            llc = true;
+        }
+        vcpkg = vcpkg or pkg.vcpkg;
+    }
+    if (llc) exe.linkLibC();
+    if (builtin.os.tag == .windows and vcpkg) exe.addVcpkgPaths(.static) catch |err| @panic(@errorName(err));
+}
+
+pub const Package = struct {
+    directory: string,
+    pkg: ?Pkg = null,
+    c_include_dirs: []const string = &.{},
+    c_source_files: []const string = &.{},
+    c_source_flags: []const string = &.{},
+    system_libs: []const string = &.{},
+    frameworks: []const string = &.{},
+    vcpkg: bool = false,
+};
+
+fn checkMinZig(current: std.SemanticVersion, exe: *std.build.LibExeObjStep) void {
+    const min = std.SemanticVersion.parse("null") catch return;
+    if (current.order(min).compare(.lt)) @panic(exe.builder.fmt("Your Zig version v{} does not meet the minimum build requirement of v{}", .{current, min}));
+}
+
+pub const dirs = struct {
+    pub const _root = "";
+    pub const _ybgp4lqar9a3 = cache ++ "/../..";
+    pub const _l21900j9ct9e = cache ++ "/git/github.com/jecolon/ziglyph";
+};
+
+pub const package_data = struct {
+    pub const _ybgp4lqar9a3 = Package{
+        .directory = dirs._ybgp4lqar9a3,
+    };
+    pub const _l21900j9ct9e = Package{
+        .directory = dirs._l21900j9ct9e,
+        .pkg = Pkg{ .name = "ziglyph", .source = .{ .path = dirs._l21900j9ct9e ++ "/src/ziglyph.zig" }, .dependencies = null },
+    };
+    pub const _root = Package{
+        .directory = dirs._root,
+    };
+};
+
+pub const packages = &[_]Package{
+    package_data._l21900j9ct9e,
+};
+
+pub const pkgs = struct {
+    pub const ziglyph = package_data._l21900j9ct9e;
+};
+
+pub const imports = struct {
+};
