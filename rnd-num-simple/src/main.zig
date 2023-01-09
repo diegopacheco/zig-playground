5abaa4be8be5dae4ba1acc847e7cbf22336f9539
diegopacheco <diego.pacheco.it@gmail.com>
2023-01-08 18:31:10 -0800

    rnd

---
+++ rnd-num-simple/src/main.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.os.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    const rand = prng.random();
+
+    const a = rand.float(f32);
+    const b = rand.boolean();
+    const c = rand.int(u8);
+    const d = rand.intRangeAtMost(u8, 0, 255);   
+
+    std.debug.print("a {} b {} c {} d {}",.{a,b,c,d});
+
+}
