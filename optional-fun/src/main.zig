952cb6b801f7b72951dbf31638afa5b5d36beea8
diegopacheco <diego.pacheco.it@gmail.com>
2023-06-30 16:32:30 -0700

    .

--- optional-fun/src/main.zig
+++ optional-fun/src/main.zig
@@ -2,21 +2,19 @@ const std = @import("std");
 const expect = std.testing.expect;
 
 pub fn main() !void {
-    
     var found_index: ?usize = null;
     const data = [_]i32{ 1, 2, 3, 4, 5, 6, 7, 8, 12 };
-    for (data) |v, i| {
+    for (data, 0..) |v, i| {
         if (v == 10) found_index = i;
     }
     try expect(found_index == null);
-    std.debug.print("Searching for 10 in {any} - found ? {any}.\n", .{data,found_index});
-    
+    std.debug.print("Searching for 10 in {any} - found ? {any}.\n", .{ data, found_index });
 }
 
 test "optional" {
     var found_index: ?usize = null;
     const data = [_]i32{ 1, 2, 3, 4, 5, 6, 7, 8, 12 };
-    for (data) |v, i| {
+    for (data, 0..) |v, i| {
         if (v == 10) found_index = i;
     }
     try expect(found_index == null);
