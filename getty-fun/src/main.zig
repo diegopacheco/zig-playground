b670e7a94e5d09cd6ae259615905d9ec02e80ff1
diegopacheco <diego.pacheco.it@gmail.com>
2023-07-20 20:15:08 -0700

    .

---
+++ getty-fun/src/main.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+const json = @import("json");
+
+const allocator = std.heap.page_allocator;
+
+const Point = struct { x: i32, y: i32 };
+
+pub fn main() anyerror!void {
+    const point = Point{ .x = 1, .y = 2 };
+
+    const string = try json.toSlice(allocator, point);
+    defer allocator.free(string);
+
+    // {"x":1,"y":2}
+    std.debug.print("{s}\n", .{string});
+}
