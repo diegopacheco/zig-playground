f906213473559f766e671fe569a385b428a358f7
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-28 00:08:30 -0700

    zbench with zig

--- zbench-fun/src/main.zig
+++ zbench-fun/src/main.zig
@@ -2,15 +2,20 @@ const std = @import("std");
 const zbench = @import("zbench");
 
 fn benchmarkMyFunction(b: *zbench.Benchmark) void {
-    _ = b;
     var i = 0;
     while (i <= 3000) {
         i += 1;
     }
+    b.incrementOperations(1);
 }
 
 pub fn main() !void {
     var allocator = std.heap.page_allocator;
+    var resultsAlloc = std.ArrayList(zbench.BenchmarkResult).init(allocator);
     var b = try zbench.Benchmark.init("benchmarkMyFunction", &allocator);
-    try zbench.run(benchmarkMyFunction, &b);
+    var benchmarkResults = zbench.BenchmarkResults{
+        .results = resultsAlloc,
+    };
+
+    try zbench.run(benchmarkMyFunction, &b, &benchmarkResults);
 }
