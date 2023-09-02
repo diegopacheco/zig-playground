3a86ca212c2bc51d30d422f68a8f54396452427f
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-01 22:25:23 -0700

    Slice for loop

--- slice-for-loop-fun/src/main.zig
+++ slice-for-loop-fun/src/main.zig
@@ -2,13 +2,20 @@ const std = @import("std");
 
 pub fn main() !void {
     
+    std.debug.print("String Array and Slice \n",.{});
     const message = "Zig Rocks";
-    std.debug.print("{s}", .{message});
+    std.debug.print("{s}\n", .{message});
 
-    var slice = message[3..message.len];
-
+    var slice = message[4..message.len];
     for(slice) |value| {
         std.debug.print("value [{c}] \n", .{value});
+    }
+
+    std.debug.print("Number Array and Slice \n",.{});
+    const array = [_]u32{10,20,30,40,50,60};
+    const arraySlice = array[0..array.len];
+    for(arraySlice) |value| {
+        std.debug.print("value [{d}] \n", .{value});
     }
 
 }
