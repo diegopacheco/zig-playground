a19df55b2eb3cf76e691975911d3eb01ec703ca3
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-01 22:20:29 -0700

    Slice for loop

--- slice-for-loop-fun/src/main.zig
+++ slice-for-loop-fun/src/main.zig
@@ -5,11 +5,10 @@ pub fn main() !void {
     const message = "Zig Rocks";
     std.debug.print("{s}", .{message});
 
-}
+    var slice = message[3..message.len];
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    for(slice) |value| {
+        std.debug.print("value [{c}] \n", .{value});
+    }
+
 }
