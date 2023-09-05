8b2af11090359ef5c83d8eb1ba0c6fac5d99bc64
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-04 23:02:40 -0700

    poc with json and getty

--- json-lib-fun/src/main.zig
+++ json-lib-fun/src/main.zig
@@ -2,17 +2,41 @@ const std = @import("std");
 const json = @import("json");
 
 const allocator = std.heap.page_allocator;
-const Point = struct { 
-    x: i32, 
-    y: i32 
-};
+const Point = struct { x: i32, y: i32 };
 
-pub fn main() anyerror!void {
+fn serialization() anyerror!void {
+    std.debug.print("Serialization === \n", .{});
+
     const point = Point{ .x = 1, .y = 2 };
-
     const string = try json.toSlice(allocator, point);
     defer allocator.free(string);
 
     // {"x":1,"y":2}
     std.debug.print("{s}\n", .{string});
+
+    // {
+    //   "x": 1,
+    //   "y": 2
+    // }
+    std.debug.print("{s}\n", .{try json.toPrettySlice(allocator, point)});
+}
+
+fn deserialization() anyerror!void {
+    std.debug.print("Derialization === \n", .{});
+
+    const stringJson =
+        \\{
+        \\  "x": 1,
+        \\  "y": 2
+        \\}
+    ;
+    const pointFromJson = try json.fromSlice(allocator, Point, stringJson);
+
+    // main.Point{ .x = 1, .y = 2 }
+    std.debug.print("{any}\n", .{pointFromJson});
+}
+
+pub fn main() anyerror!void {
+    try serialization();
+    try deserialization();
 }
