f6e812baac02e48ba874fb194797e66c7b97d65b
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-05 01:50:56 -0700

    zig arena

---
+++ arena-allocator-fun/src/main.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+pub fn main() !void {
+    
+}
+
+test "simple arena allocator test" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    _ = try allocator.alloc(u8, 1);
+    _ = try allocator.alloc(u8, 10);
+    _ = try allocator.alloc(u8, 100);
+}
