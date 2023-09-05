4fc63809b23f2af8c1eae03cf4861aa0017c4961
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-04 22:06:34 -0700

    poc with json and getty

--- json-lib-fun/build.zig
+++ json-lib-fun/build.zig
@@ -24,11 +24,17 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    //const getty_module = b.addModule("getty", .{ .source_file = .{ .path = "libs/getty/src/getty.zig" } });
-    //exe.addModule("getty", getty_module);
+    const getty_module = b.addModule("getty", .{ .source_file = .{ .path = "libs/getty/src/getty.zig" } });
+    exe.addModule("getty", getty_module);
 
     const json_module = b.addModule("json", .{ .source_file = .{ .path = "libs/json/src/json.zig" } });
     exe.addModule("json", json_module);
+
+    const ser_module = b.addModule("json", .{ .source_file = .{ .path = "libs/json/src/ser.zig" } });
+    exe.addModule("json", ser_module);
+
+    const de_module = b.addModule("json", .{ .source_file = .{ .path = "libs/json/src/de.zig" } });
+    exe.addModule("json", de_module);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
