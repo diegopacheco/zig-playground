86991d7c39f6dd6233d03ee20481c64cb35f2081
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-05 01:55:26 -0700

    zig arena allocator and array list

--- arena-allocator-fun/src/main.zig
+++ arena-allocator-fun/src/main.zig
@@ -1,7 +1,17 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
 
 pub fn main() !void {
-    
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var list = ArrayList(u8).init(allocator);
+    defer list.deinit();
+    try list.appendSlice("Hello World!");
+
+    std.debug.print("{s}",.{list.items});
 }
 
 test "simple arena allocator test" {
