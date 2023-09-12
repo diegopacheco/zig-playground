6873a4873e1a68fce50fcd96eb3d9e711816b6af
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-11 23:37:22 -0700

    bytes eq

--- string-equals/src/main.zig
+++ string-equals/src/main.zig
@@ -14,11 +14,24 @@ const Person = struct {
         };
     }
 
-    pub fn equals(self:main.Person,other:Person) bool {
-        return std.mem.eql(main.Person, self, other);
+    pub fn equals(self: Person,other: Person) bool {
+        return 
+               self.id == other.id and 
+               std.mem.eql(u8, self.name, other.name) and
+               std.mem.eql(u8, self.email, other.email);
     }
 
 };
+
+fn check(p1:Person,p2:Person) void {
+    print("p1 type is {any}", .{@TypeOf(p1)});
+    print("p2 type is {any}", .{@TypeOf(p2)});
+    if (p1.equals(p2)) {
+        print("Yes! Same spider man \n",.{});
+    }else{
+        print("Nooo! Different spider man. different multiverse \n",.{});
+    }
+}
 
 pub fn main() !void {
 
@@ -33,13 +46,14 @@ pub fn main() !void {
         .name = "Petter",
         .email = "spider@man.com"
     };
+    
+    const venon = Person{
+        .id = 1,
+        .name = "Venon",
+        .email = "spider@man.com"
+    };
 
-    print("Person type is {any}", .{@TypeOf(spiderA)});
-
-    if (spiderA.equals(spiderB)) {
-        print("Same spider man",.{});
-    }else{
-        print("Different spider man. different multiverse",.{});
-    }
+    check(spiderA, spiderB);
+    check(spiderA, venon);    
 
 }
