7d1afc4110460b1ca96a5710eccecc7ffed3daa7
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-11 23:59:06 -0700

    zig range for and indexed for

--- range-for-index-for/src/main.zig
+++ range-for-index-for/src/main.zig
@@ -1,6 +1,12 @@
 const std = @import("std");
 
 pub fn main() !void {
-        
+    for (0..10) |i| {
+        std.debug.print("{d} ", .{i});
+    }
+
+    for (0..10, 0..) |i, index| {
+        std.debug.print("value: {d} index: {d} \n", .{i,index});
+    }
 }
 
