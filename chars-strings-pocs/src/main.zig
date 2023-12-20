5d898e253c527c28d1def6f7ce42bcb2d92127bd
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-20 01:10:50 -0800

    zig poc to figure types and some conversions between types

--- chars-strings-pocs/src/main.zig
+++ chars-strings-pocs/src/main.zig
@@ -1,11 +1,13 @@
 const std = @import("std");
 const print = @import("std").debug.print;
+const mem = @import("std").mem;
 
 pub fn main() !void {
     char_type();
     direct_concat();
     replace_string_with_char();
     try string_to_int();
+    compare_strings();
     concat_string_with_char();
 }
 
@@ -34,7 +36,19 @@ fn replace_string_with_char() void {
     debug_type(t);
 }
 
-fn concat_string_with_char() void {}
+fn compare_strings() void {
+    var result = mem.eql(u8, "hello", "h\x65llo");
+    if (result) {
+        print("2 strings are equal! \n", .{});
+    }
+}
+
+fn concat_string_with_char() void {
+    //var s = "Hello World";
+    //const c = '!';
+    //const result = concat(c, s);
+    //debug_type(result);
+}
 
 fn debug_type(t: anytype) void {
     print("Type is {}\n", .{@TypeOf(t)});
