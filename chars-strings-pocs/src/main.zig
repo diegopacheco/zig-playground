ba4a95084ab25c4d5acae5b2fc3426d4859943db
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-20 00:38:56 -0800

    zig poc to figure types and some conversions between types

--- chars-strings-pocs/src/main.zig
+++ chars-strings-pocs/src/main.zig
@@ -2,15 +2,39 @@ const std = @import("std");
 const print = @import("std").debug.print;
 
 pub fn main() !void {
-    const str = "Aaa" ++ "B";
-    debug_type(str);
+    char_type();
+    direct_concat();
+    replace_string_with_char();
+    try string_to_int();
+    concat_string_with_char();
+}
 
-    var str_temp = "1234567890";
-    debug_type(str_temp);
-
+fn char_type() void {
     const c = 'A';
     debug_type(c);
 }
+
+fn direct_concat() void {
+    const str = "Aaa" ++ "B";
+    debug_type(str);
+}
+
+fn string_to_int() !void {
+    const str = "22";
+    const integer = try std.fmt.parseInt(i32, str, 10);
+    print("{} converted from string ", .{integer});
+    debug_type(integer);
+}
+
+fn replace_string_with_char() void {
+    var s = "good morning";
+    var t = s.*;
+    t[0] = 'm';
+    std.debug.print("{s} - ", .{t});
+    debug_type(t);
+}
+
+fn concat_string_with_char() void {}
 
 fn debug_type(t: anytype) void {
     print("Type is {}\n", .{@TypeOf(t)});
