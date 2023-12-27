2486383b55cdfd7a0852a4999cf53bb0cc81f265
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-27 01:39:43 -0800

    socket sever in zig

--- simple-server-socket-zig/src/main.zig
+++ simple-server-socket-zig/src/main.zig
@@ -2,13 +2,13 @@ const std = @import("std");
 const print = std.debug.print;
 const net = std.net;
 
-const addr = net.Address.initIp4(.{ 127, 0, 0, 1 }, 7496);
+const addr = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, 7496);
 pub fn main() !void {
     const options = net.StreamServer.Options{};
-    const server = net.StreamServer.init(options);
+    var server = net.StreamServer.init(options);
 
     _ = try server.listen(addr);
-    print("Socket Server listening on port: {s}\n", .{addr});
+    print("Socket Server listening on port: {any}\n", .{addr});
 
     while (true) {
         const client = try server.accept();
@@ -19,7 +19,7 @@ pub fn main() !void {
         _ = try stream.read(&buffer);
         _ = try stream.write("Hey from the server");
 
-        print("Socket client addr is: {s}\n", .{client_addr});
+        print("Socket client addr is: {any}\n", .{client_addr});
         print("Request buffer is: {s}\n", .{buffer});
     }
 }
