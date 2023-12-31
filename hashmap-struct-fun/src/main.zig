e166000cd915c6fb6ff1112a327d88e17290e3c7
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-31 00:09:56 -0800

    hashmap with allocator and struct

--- hashmap-struct-fun/src/main.zig
+++ hashmap-struct-fun/src/main.zig
@@ -84,10 +84,3 @@ pub fn main() !void {
     print("DAO has Andrew? {any}\n", .{dao.has(2)});
     print("DAO has {d} size \n", .{dao.count()});
 }
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
