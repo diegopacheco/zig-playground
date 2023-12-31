e1318ca457ecde017b6442ff808c556d76d9527a
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-31 00:07:18 -0800

    hashmap with allocator and struct

--- hashmap-struct-fun/src/main.zig
+++ hashmap-struct-fun/src/main.zig
@@ -2,12 +2,12 @@ const std = @import("std");
 const print = std.debug.print;
 
 const Person = struct {
-    alllocator: std.men.allocator,
+    allocator: std.mem.Allocator,
     id: usize,
     name: []u8,
     email: []u8,
 
-    fn new(allocator: std.men.Allocator, id: usize, name: []u8, email: []u8) !Person {
+    fn new(allocator: std.mem.Allocator, id: usize, name: []const u8, email: []const u8) !Person {
         return .{
             .allocator = allocator,
             .id = id,
@@ -45,9 +45,45 @@ const PersonDAO = struct {
     fn remove(self: *PersonDAO, id: usize) ?Person {
         return if (self.map.fetchRemove(id)) |p| p.value else null;
     }
+
+    fn has(self: *PersonDAO, id: usize) bool {
+        if (self.map.contains(id)) {
+            return true;
+        }
+        return false;
+    }
+
+    fn count(self: *PersonDAO) usize {
+        return self.map.count();
+    }
 };
 
-pub fn main() !void {}
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var dao = PersonDAO.new(allocator);
+    defer dao.deinit();
+
+    var diego = try Person.new(allocator, 1, "Diego", "diego@diego.diego.com");
+    defer diego.deinit();
+    _ = try dao.put(diego);
+
+    var andrew = try Person.new(allocator, 2, "Andrew", "andrew@andrew.zig.com");
+    defer andrew.deinit();
+    _ = try dao.put(andrew);
+
+    if (dao.get(1)) |person| {
+        print("Got person id:{d}, name:{s} email:{s} \n", .{ person.id, person.name, person.email });
+        print("DAO has user 1 ? {any}\n", .{dao.has(1)});
+    }
+    print("DAO has {d} people \n", .{dao.count()});
+
+    _ = dao.remove(2);
+    print("DAO has Andrew? {any}\n", .{dao.has(2)});
+    print("DAO has {d} size \n", .{dao.count()});
+}
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
