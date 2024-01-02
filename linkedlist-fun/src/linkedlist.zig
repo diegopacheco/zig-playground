a9760412604fa232c69b5ad21c31e0962c89497f
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-02 00:39:06 -0800

    Generic DoubleLinkedList type in zig

--- linkedlist-fun/src/linkedlist.zig
+++ linkedlist-fun/src/linkedlist.zig
@@ -3,24 +3,142 @@ const Allocator = std.mem.Allocator;
 const dprint = std.debug.print;
 
 pub fn DoubleLinkedList(comptime T: type) type {
+    const Errors = error{
+        ElementNotFound,
+        EmptyList,
+    };
+
     const Node = struct {
         value: T,
         next: ?*Self,
         prev: ?*Self,
         const Self = @This();
     };
-    _ = Node;
 
     return struct {
         allocator: Allocator,
         count: usize,
+        head: ?*Node,
+        tail: ?*Node,
         const Self = @This();
 
         pub fn init(allocator: Allocator) !Self {
             return .{
                 .allocator = allocator,
+                .head = null,
+                .tail = null,
                 .count = 0,
             };
+        }
+
+        pub fn deinit(self: *Self) void {
+            if (self.tail) |_| {
+                var current: ?*Node = self.head;
+                while (current) |curr| {
+                    var temp: *Node = curr;
+                    current = curr.next;
+                    self.allocator.destroy(temp);
+                }
+            }
+        }
+
+        pub fn add(self: *Self, value: T) !bool {
+            var newNode: *Node = try self.allocator.create(Node);
+            newNode.value = value;
+            newNode.prev = null;
+            newNode.next = null;
+
+            if (self.tail) |safe_tail| {
+                newNode.prev = safe_tail;
+                safe_tail.next = newNode;
+                self.tail = newNode;
+            } else {
+                self.head = newNode;
+                self.tail = newNode;
+            }
+            self.count += 1;
+            return true;
+        }
+
+        pub fn get(self: *Self, index: usize) !T {
+            if (self.count == 0) {
+                return Errors.EmptyList;
+            }
+            if (index > self.count) {
+                return Errors.ElementNotFound;
+            }
+            if (self.tail) |_| {
+                var del_count: usize = 0;
+                var current: ?*Node = self.head;
+                while (current) |curr| : (current = curr.next) {
+                    if (del_count == index) {
+                        return curr.value;
+                    }
+                    del_count += 1;
+                }
+            }
+            return Errors.ElementNotFound;
+        }
+
+        pub fn remove(self: *Self, index: usize) !T {
+            if (self.count == 0) {
+                return Errors.EmptyList;
+            }
+            if (index > self.count) {
+                return Errors.ElementNotFound;
+            }
+            if (self.tail) |_| {
+                var del_count: usize = 0;
+                var current: ?*Node = self.head;
+                while (current) |curr| : (current = curr.next) {
+                    if (del_count == index) {
+                        dprint("* Found index for removal \n", .{});
+
+                        var result: T = curr.value;
+                        if (curr == self.tail) {
+                            dprint("* Removing tail \n", .{});
+                            var temp: *Node = curr;
+                            self.allocator.destroy(temp);
+                            self.tail = null;
+                        }
+                        if (curr == self.head) {
+                            dprint("* Removing head \n", .{});
+                            var temp: *Node = curr;
+                            self.allocator.destroy(temp);
+                            self.head = null;
+                        } else {
+                            dprint("* Removing in the middle of the list \n", .{});
+                            var temp: *Node = curr.next.?;
+                            temp.prev = curr.prev;
+                            curr.prev.?.next = curr.next;
+                            temp = curr;
+                            self.allocator.destroy(temp);
+                        }
+                        self.count -= 1;
+                        return result;
+                    }
+                    del_count += 1;
+                }
+            }
+            return Errors.ElementNotFound;
+        }
+
+        pub fn print(self: *Self) void {
+            if (self.tail) |_| {
+                dprint(">>> DLL size is: {d}\n", .{self.count});
+            } else {
+                dprint(">>> DLL is empty!\n", .{});
+            }
+
+            var has_elms = false;
+            var current: ?*Node = self.head;
+            while (current) |curr| : (current = curr.next) {
+                dprint(">> element: [{}] ", .{curr.value});
+                has_elms = true;
+            }
+            if (has_elms) {
+                dprint("\n", .{});
+            }
         }
     };
 }
--- linkedlist-fun/src/main.zig
+++ linkedlist-fun/src/main.zig
@@ -6,10 +6,30 @@ const IntDoubleLinkedList = ds.DoubleLinkedList(i32);
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-
     var alloc = gpa.allocator();
-    var dll = IntDoubleLinkedList.init(alloc);
-    print("Double Linked List {any}", .{dll});
+
+    var dll = try IntDoubleLinkedList.init(alloc);
+    defer dll.deinit();
+    print("Double Linked List {any}\n", .{dll});
+
+    dll.print();
+    _ = try dll.add(1);
+    _ = try dll.add(2);
+    _ = try dll.add(3);
+    _ = try dll.add(4);
+    _ = try dll.add(5);
+    _ = try dll.add(6);
+    dll.print();
+
+    var result: i32 = try dll.get(1);
+    print("Found {any} \n", .{result});
+
+    _ = dll.get(100) catch |err| {
+        print("Element not found {any} \n", .{err});
+    };
+
+    _ = try dll.remove(3);
+    dll.print();
 }
 
 test "simple test" {
