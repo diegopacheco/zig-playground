492ed12a6155de5cc1ebe336fd4c2a98d94171fb
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-06 00:10:51 -0800

    base64 encoding fun with zig

---
+++ base64-fun/src/main.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+const print = std.debug.print;
+const encoder = std.base64.standard.Encoder;
+
+const Person = struct {
+    id: usize,
+    name: []const u8,
+    mail: []const u8,
+    const Self = @This();
+
+    pub fn init(id: usize, name: []const u8, mail: []const u8) Self {
+        return .{ .id = id, .name = name, .mail = mail };
+    }
+};
+
+pub fn main() !void {
+    var jd = Person.init(1, "John", "john@doe.com");
+    print("Person id: {d}, name: {s}, email:{s}\n", .{ jd.id, jd.name, jd.mail });
+
+    var buf: [0x100]u8 = undefined;
+    _ = encoder.encode(&buf, jd.name);
+    print("Base64 {s}\n", .{buf});
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
