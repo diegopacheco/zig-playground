cffc27af051576a321f30635c6ce5273f0d1444b
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-06 16:38:14 -0800

    zig base64 fun - added more unit tests

--- base64-fun/src/main.zig
+++ base64-fun/src/main.zig
@@ -140,7 +140,7 @@ test "Person.to_decoded" {
     var bb: []u8 = try allocator.alloc(u8, 28);
     try jd.to_decoded(bb, enc);
 
-    try std.testing.expectEqualSentinel(u8, 0, "1,john,john@doe.com", bb);
+    try std.testing.expect(std.mem.containsAtLeast(u8, bb, 1, "1,john,john@doe.com"));
     allocator.free(list);
     allocator.free(enc);
     allocator.free(bb);
